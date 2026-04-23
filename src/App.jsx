5c311c36fadf00f186e67924e509ce431e5d9982
AlphaMugisha <tigerkev07@gmail.com>
2026-04-23 08:29:56 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is the most gay thing that i ever saw gng</p>
+    </div>
+  )
+}
+
+export default App
