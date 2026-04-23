d6adf04bcd1b31a40fc3f26a9a714041d25d1cfe
AlphaMugisha <tigerkev07@gmail.com>
2026-04-23 08:49:38 +0200

    adding another part

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
+import ProductCard from "./components/ProductCard";
+import products from "./data/Products";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+
+      <ProductCard product={products[0]} />
+
       <Footer />
     </>
   );
---
+++ src/data/Products.js
@@ -0,0 +1,10 @@
+const products = [
+  {
+    id: 1,
+    name: "Bananas",
+    price: 1200,
+    image: "https://placehold.co/200x150"
+  }
+];
+
+export default products;
