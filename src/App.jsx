702ed991f41174819b55a427187fbfdf74a9a89b
AlphaMugisha <tigerkev07@gmail.com>
2026-04-23 09:07:31 +0200

    adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 import ProductCard from "./components/ProductCard";
-import products from "./data/Products";
-import { useState } from "react";
+import products from "./data/Products"; 
+import "./App.css";
 
 function App() {
+  const [showSpecial, setShowSpecial] = useState(false);
+
   return (
     <>
       <Header />
       <Hero />
 
-      <ProductCard product={products[0]} />
+      <button
+        className="special-btn"
+        onClick={() => setShowSpecial(!showSpecial)}
+      >
+        {showSpecial ? "Hide Today's Special" : "Show Today's Special"}
+      </button>
+
+      {showSpecial && <ProductCard product={products[0]} />}
 
       <Footer />
     </>
